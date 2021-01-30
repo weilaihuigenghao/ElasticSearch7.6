175fa7ba9f17549e64f8f6f8e6ba4dc27aaffe36
2123562127 <y2123562127@163.com>
2021-01-30 12:38:35 +0800

    修改Test01.java

--- src/test/java/com/kuang/Test01.java
+++ src/test/java/com/kuang/Test01.java
@@ -3,5 +3,7 @@ package com.kuang;
 public class Test01 {
     public static void main(String[] args) {
         System.out.println("添加到GitHub");
+        System.out.println("添加到GitHub");
+        System.out.println("添加到GitHub");
     }
 }
